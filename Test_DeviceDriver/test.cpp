a7293876b95c06369315d83161811182737bdef0
HyunMin Cho <hm1041.cho@samsung.com>
2024-06-03 10:42:57 +0900

    [feature] Implement Device Driver

--- Test_DeviceDriver/test.cpp
+++ Test_DeviceDriver/test.cpp
@@ -2,9 +2,53 @@
 #include "gmock/gmock.h"
 
 #include "../TDD_DeviceDriver_KATA/device_driver.cpp"
+#include "mock_flash_memory_device.cpp"
 
-TEST(DeviceDriverTest, DummyTest) {
-	DeviceDriver app(nullptr);
+using testing::_;
+using testing::Return;
+
+TEST(DeviceDriverTest, ReadZeroTest) {
+	MockFlashMemoryDevice mock_device;
+	EXPECT_CALL(mock_device, read(_))
+		.Times(5)
+		.WillRepeatedly(Return(0));
+
+	DeviceDriver app(&mock_device);
+	int expected = 0x00;
+	int actual = app.read(0x00);
+
+	EXPECT_EQ(actual, expected);
+}
+
+TEST(DeviceDriverTest, ReadFailExceptionTest) {
+	MockFlashMemoryDevice mock_device;
+	EXPECT_CALL(mock_device, read(_))
+		.WillOnce(Return(1))
+		.WillRepeatedly(Return(0x00));
+
+	DeviceDriver app(&mock_device);
+
+	EXPECT_THROW(app.read(0x00), ReadFailException);
+}
+
+TEST(DeviceDriverTest, WriteAtEmptyArea) {
+	MockFlashMemoryDevice mock_device;
+	EXPECT_CALL(mock_device, read(_))
+		.Times(5)
+		.WillRepeatedly(Return(0xFF));
+
+	DeviceDriver app(&mock_device);
+	app.write(0x00, 0x05);
+}
+
+TEST(DeviceDriverTest, WriteAtAlreadyFilledArea) {
+	MockFlashMemoryDevice mock_device;
+	EXPECT_CALL(mock_device, read(_))
+		.Times(5)
+		.WillRepeatedly(Return(0));
+
+	DeviceDriver app(&mock_device);
+	EXPECT_THROW(app.write(0x00, 0x05), WriteFailException);
 }
 
 
