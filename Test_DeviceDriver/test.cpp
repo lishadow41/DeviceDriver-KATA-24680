4ba690387593a0b56dc6b38e2abe9a732517e9d6
HyunMin Cho <hm1041.cho@samsung.com>
2024-06-03 10:38:35 +0900

    [config] update to use gMock

--- Test_DeviceDriver/test.cpp
+++ Test_DeviceDriver/test.cpp
@@ -1,6 +1,18 @@
-#include "pch.h"
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
 #include "../TDD_DeviceDriver_KATA/device_driver.cpp"
 
 TEST(DeviceDriverTest, DummyTest) {
 	DeviceDriver app(nullptr);
+}
+
+
+GTEST_API_ int main(int argc, char** argv) {
+	std::cout << "Running main() from gmock_main.cc\n";
+	// Since Google Mock depends on Google Test, InitGoogleMock() is
+	// also responsible for initializing Google Test.  Therefore there's
+	// no need for calling testing::InitGoogleTest() separately.
+	testing::InitGoogleMock(&argc, argv);
+	return RUN_ALL_TESTS();
 }
